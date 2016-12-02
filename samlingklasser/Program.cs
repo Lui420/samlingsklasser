0f40f1810316598d17b7443fc724cf5a0ea9d2a5
Lui420 <lewi.lindstrom@gmail.com>
2016-12-02 14:42:33 +0100

    ee

--- samlingklasser/Program.cs
+++ samlingklasser/Program.cs
@@ -10,6 +10,31 @@ namespace samlingklasser
     {
         static void Main(string[] args)
         {
+            //ovning 1
+            Dictionary<int, int> resultat = new Dictionary<int, int>();
+
+            
+            Random random = new Random();
+
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                
+                int tal = random.Next(1, 7);
+
+                
+                if (!resultat.ContainsKey(tal))
+                    resultat.Add(tal, 0);
+
+                
+                resultat[tal]++;
+            }
+
+            
+            foreach (KeyValuePair<int, int> kvp in resultat)
+            {
+                Console.WriteLine("Nyckel: {0} Värde: {1}", kvp.Key, kvp.Value);
+            }
         }
     }
 }
